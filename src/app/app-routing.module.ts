5ce7dd29d421f1dcf92cb56f5f2ce5ed3d3e1b51
ShekharBajetha <bajethashekhar@gmail.com>
2021-05-17 22:16:41 +0530

    Add routing modules changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EmailComponent } from './components/email/email.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"sendemail",
+    component:EmailComponent,
+    pathMatch:"full"
+  },
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
