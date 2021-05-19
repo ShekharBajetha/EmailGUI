0931a358edb90b7c05819e0c68ceccbfc033f469
ShekharBajetha <bajethashekhar@gmail.com>
2021-05-19 20:59:44 +0530

    Changes in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { EmailComponent } from './components/email/email.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { EmailService } from './service/email.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 
@@ -16,16 +25,23 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule, MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
-  providers: [MatSnackBar],
+  providers: [MatSnackBar, EmailService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
